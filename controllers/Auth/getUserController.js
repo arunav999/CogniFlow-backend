b630a5dcb0ae82f87b7169dde428794f8f7b90e9
Arunav Singh <arunavsingh999@gmail.com>
2025-07-11 22:53:25 +0530

    auth routes completed

--- controllers/Auth/getUserController.js
+++ controllers/Auth/getUserController.js
@@ -1,9 +1,3 @@
-// User Model
-import User from "../../models/User.js";
-
-// Error
-import ApiError from "../../errors/Apierror.js";
-
 // Constants
 import { STATUS_CODES } from "../../constants/statusCodes.js";
 
