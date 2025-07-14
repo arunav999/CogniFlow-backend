eab2b5ee7944752ff916994f645eca8ce2ec09f3
Arunav Singh <arunavsingh999@gmail.com>
2025-07-14 13:47:55 +0530

    ticket deleted success

--- controllers/Tickets/deleteTicketController.js
+++ controllers/Tickets/deleteTicketController.js
@@ -1,6 +1,41 @@
+// Error
+import ApiError from "../../errors/Apierror.js";
+
 // Constants
 import { STATUS_CODES } from "../../constants/statusCodes.js";
 
 // Models
+import Project from "../../models/Project.js";
+import Ticket from "../../models/Ticket.js";
 
-export const deleteTicketByIdController = async (req, res, next) => {};
+export const deleteTicketByIdController = async (req, res, next) => {
+  const userId = req.user._id;
+  const ticketId = req.params.id;
+
+  try {
+    // === Validation: Check if ticket exists ===
+    const findTicket = await Ticket.findById(ticketId);
+    // if no ticket
+    if (!findTicket)
+      return new ApiError(STATUS_CODES.NOT_FOUND, "No ticket found", "");
+
+    // === Cascade delete: Remove all related comments ===
+
+    // === Remove ticket reference from project ===
+    await Project.updateMany(
+      { tickets: ticketId },
+      { $pull: { tickets: ticketId } }
+    );
+
+    // === Delete project ===
+    await Ticket.findByIdAndDelete(ticketId);
+
+    // === Response ===
+    res.status(STATUS_CODES.OK).json({
+      success: true,
+      message: "Ticket and related data deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- errors/errorHandler.js
+++ errors/errorHandler.js
@@ -4,6 +4,7 @@ const errorHandler = (err, req, res, next) => {
   const status = err.statusCode || STATUS_CODES.SERVER_ERROR;
 
   const errorResponse = {
+    success: false,
     message: err.message || "Somenting went wrong",
   };
 
